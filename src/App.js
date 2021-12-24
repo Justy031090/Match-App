96505aad0ce6487c15b7eba7c77845e402f7319c
justy031090 <justy031090@gmail.com>
2021-12-24 20:33:39 +0200

    match-app light css

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import DogImages from './components/DogImages';
+
+function App() {
+    return (
+        <div>
+            <DogImages />
+        </div>
+    );
+}
+export default App;
---
+++ src/components/DogImages.jsx
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import dogsApi from '../api/dogsApi';
+import './dog-image.css';
+import dogLoveImage from '../assets/Dog-Love.jpg';
+
+export class DogImages extends Component {
+    state = {
+        dogImages: [],
+        dogUrl: 'Lets check if you are a dog lover !',
+        index: 0,
+        like: 0,
+        dislike: 0,
+        loading: false,
+    };
+    async componentDidMount() {
+        const data = await dogsApi.get('/breeds/image/random/10');
+        this.setState({
+            dogImages: data.data.message,
+        });
+    }
+    renderCard = () => {
+        const rendered = this.state.dogImages.map((image, index) => {
+            return <img key={index} src={image} alt="https://http.cat/404" />;
+        });
+        if (this.state.index < 10) {
+            return this.setState({
+                dogUrl: rendered[this.state.index],
+                index: this.state.index + 1,
+            });
+        } else {
+            if (this.state.like > this.state.dislike) {
+                this.setState({
+                    dogUrl: (
+                        <div>
+                            <img src={dogLoveImage} alt="" />
+                            <span>I love you too !</span>
+                        </div>
+                    ),
+                });
+            } else
+                this.setState({
+                    dogUrl: (
+                        <div>
+                            <img src="https://http.cat/409" alt="Not Found" />
+                        </div>
+                    ),
+                });
+        }
+    };
+    likeHandle = () => {
+        this.renderCard();
+        this.setState({ like: this.state.like + 1, loading: false });
+    };
+    dislikeHandle = () => {
+        this.setState({ loading: true });
+        this.renderCard();
+        this.setState({ dislike: this.state.dislike + 1, loading: false });
+    };
+    render() {
+        return (
+            <div>
+                <div className="counters">
+                    <span className="like fa fa-thumbs-up">
+                        {this.state.like}
+                    </span>
+                    <span className="dislike 	fa fa-thumbs-down">
+                        {this.state.dislike}
+                    </span>
+                </div>
+                <div className="image-container">{this.state.dogUrl}</div>
+                {this.state.loading && (
+                    <div className="no-spinner">Loading...</div>
+                )}
+                <div className="buttons">
+                    <button onClick={() => this.likeHandle()}>
+                        <i className="fa fa-check"> </i>
+                    </button>
+                    <button onClick={() => this.dislikeHandle()}>
+                        <i className="fa fa-close"></i>
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default DogImages;
